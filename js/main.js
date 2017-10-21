4bb40a570c6e8df1554b9b8fdad118edf4aa8c44
Richard Spence <richardspence@Richards-MacBook-Air.local>
2017-10-20 19:45:32 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Binary Search
+//Search for a given value (key) inside of a list (numArray)
+//Run in O(log n) run time - very perfornmant
+//Can be written as an recursive function
+
+/*
+============================================
+recursion review example
+============================================
+
+function factorial(num){
+  if (num === 1){
+  return num;
+} else {
+  return num * factorial(num - 1);
+}
+}
+*/
+
+
+function binarySearch(numArray, key){
+  var middleIdx = Math.floor(numArray.length / 2);
+  var middleElem = numArray[middleIdx];
+
+  if(middleElem === key) return true;
+  else if (middleElem < key && numArray.length > 1){
+    return binarySearch(numArray.splice(middleIdx, numArray.length), key);
+  }
+  else if (middleElem > key && numArray.length < 1){
+    return binarySearch(numArray.splice(0,middleIdx), key)
+  }
+  else return false;
+};
+
+
+binarySearch([5,7,12,16,36,39,42,56,71],56);
